4c23f73daa918ac824cd91865793d62735b1b64c
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-09-08 15:10:01 +0500

    set footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='h-[64px] p-8 bg-[#222C3C] flex justify-center items-center w-full'>
         
-     <p className='font-poppins text-sm text-[#98A6BA]'>2021 © RedStar Hospital Template By       <span className='text-[#337AB7]'> Redstartheme </span> </p>
+     <p className='font-poppins text-sm text-[#98A6BA] text-center'>2021 © RedStar Hospital Template By       <span className='text-[#337AB7]'> Redstartheme </span> </p>
 
     </div>
   )
